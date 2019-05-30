08595cfb0be7dc006dc36b82587ef200b524f64d
Cameron Browning <camb416@gmail.com>
2019-05-30 19:59:18 -0400

    fixed npm issues though gulp 4 broke everything.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,6 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch(["*.html", "js/*.js"]).on('change', browserSync.reload);
-
-});
-
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("scss/*.scss")
@@ -22,4 +10,27 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass'), function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", gulp.series('sass'))
+    gulp.watch(["*.html", "js/*.js"]).on('change', browserSync.reload);
+
+});
+
+gulp.task('default',gulp.series('sass'), function(){
+    // do nothing;
+    return null;
+});
+
+
+
 gulp.task('default', ['serve']);
+
+// THIS IS BROKEN AND IM FRUSTRATED
+// wipe this from the earth and start from scratch with something like this: https://github.com/gulpjs/gulp/blob/master/docs/recipes/minimal-browsersync-setup-with-gulp4.md
+// another day. This is old anyways 
